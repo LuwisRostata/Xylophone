91a0d85b0b4a8dfde51577f69983b8a653d4e6b9
LuwisRostata <RCD0891@dlsud.edu.ph>
2023-12-10 20:14:17 +0800

    Xylophone Version 1
    Features:
    - ordinary xylophone app features
    - implemented using keys function
    - additional random note button
    - Random note button randomly changes color and note for every press

---
+++ android/app/src/main/kotlin/com/example/rostata_xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rostata_xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(const XylophoneApp());
+
+void playSound(int A){
+  final player = AudioPlayer();
+  player.play(AssetSource('note$A.wav'));
+}
+
+Expanded keys(int A, Color B){
+  return Expanded(
+      child: TextButton(
+          style: TextButton.styleFrom(
+              backgroundColor: B,
+              shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+              enableFeedback: false
+          ),
+          onPressed: (){
+            playSound(A);
+          },
+          child: Container()
+      )
+  );
+}
+Color RandomC=Colors.purple;
+
+class XylophoneApp extends StatefulWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+class _XylophoneAppState extends State<XylophoneApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              keys(1,Colors.red),
+              keys(2,Colors.orange),
+              keys(3,Colors.yellow),
+              keys(4,Colors.lightGreen),
+              keys(5,Colors.green),
+              keys(6,Colors.blue),
+              keys(7,Colors.indigo),
+              Expanded(
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: RandomC,
+                          shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+                          enableFeedback: false
+                      ),
+                      onPressed: (){
+                        playSound(Random().nextInt(7));
+                        setState(() {
+                          RandomC=Colors.primaries[Random().nextInt(Colors.primaries.length)];
+                        });
+                      },
+                        child: const Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                          'Play random note',
+                          style: TextStyle(
+                              fontFeatures: <FontFeature>[FontFeature.enable('smcp')],
+                              fontSize:30,
+                              color: Colors.white),
+                        ),
+                        ),
+                  )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
